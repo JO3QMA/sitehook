a9be984e0409aca636af00833b4cda58e8ce7efa
JO3QMA <gumi@jo3qma.com>
2024-05-18 06:00:03 +0900

    [Fix] Sitemap.fetch test method name

--- test/test_sitemap.rb
+++ test/test_sitemap.rb
@@ -8,11 +8,11 @@ require_relative '../core/sitemap'
 
 class SitemapTest < Minitest::Test
   describe 'Fetch Sitemap.xml' do
-    def test_fetch_sitemap_response_code_200
+    def test_fetch_sitemap_response_code200
       assert_equal '200', Sitemap.fetch('https://diary.jo3qma.com/sitemap.xml').code
     end
 
-    def test_fetch_sitemap_response_code_404
+    def test_fetch_sitemap_response_code404
       assert_equal '404', Sitemap.fetch('https://diary.jo3qma.com/sitemap_none.xml').code
     end
   end
