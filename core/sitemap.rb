37ce22272f8d9fabb73722b944e8c469728e66a6
JO3QMA <gumi@jo3qma.com>
2024-05-18 05:54:17 +0900

    [Clean] Sitemap.fetch

--- core/sitemap.rb
+++ core/sitemap.rb
@@ -9,13 +9,9 @@ class Sitemap
     url = URI.parse(uri)
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
-    https.ca_file = '/usr/lib/ssl/cert.pem'
     https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    https.verify_depth = 5
-    response = ''
     https.start do
-      response = https.get(url.path)
+      https.get(url.path)
     end
-    response
   end
 end
