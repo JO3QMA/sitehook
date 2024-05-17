9b8c61f2f12558fad010592d12e00da77194f7d9
JO3QMA <gumi@jo3qma.com>
2024-05-18 05:45:24 +0900

    [Add] Sitemap class fetch test

---
+++ test/test_sitemap.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'minitest/reporters'
+Minitest::Reporters.use!
+require 'minitest/autorun'
+
+require_relative '../core/sitemap'
+
+class SitemapTest < Minitest::Test
+  describe 'Fetch Sitemap.xml' do
+    def test_fetch_sitemap_response_code_200
+      assert_equal '200', Sitemap.fetch('https://diary.jo3qma.com/sitemap.xml').code
+    end
+
+    def test_fetch_sitemap_response_code_404
+      assert_equal '404', Sitemap.fetch('https://diary.jo3qma.com/sitemap_none.xml').code
+    end
+  end
+end
