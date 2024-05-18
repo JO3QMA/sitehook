7a77d96773d9694903c48cafa2ac4336f9bc32cd
JO3QMA <gumi@jo3qma.com>
2024-05-19 06:20:57 +0900

    [Change] リファクタリング

--- test/test_sitemap.rb
+++ test/test_sitemap.rb
@@ -8,20 +8,23 @@ require 'webmock/minitest'
 require_relative '../core/sitemap'
 
 class SitemapTest < Minitest::Test
-  describe 'Fetch Sitemap.xml' do
+  describe 'Fetch sitemap.xml' do
     def setup
-      stub_request(:get, 'https://www.example.com/sitemap.xml')
+      @valid_url = 'https://www.example.com/sitemap.xml'
+      @invalid_url = 'https://404.example.com/sitemap.xml'
+      stub_request(:get, @valid_url)
         .to_return(status: 200, body: '', headers: {})
-      stub_request(:get, 'https://404.example.com/sitemap.xml')
+      stub_request(:get, @invalid_url)
         .to_return(status: 404, body: '', headers: {})
+      @sitemap = Sitemap.new
     end
 
     def test_fetch_sitemap_response_code200
-      assert_equal '200', Sitemap.fetch('https://www.example.com/sitemap.xml').code
+      assert_equal '200', @sitemap.fetch(@valid_url).code
     end
 
     def test_fetch_sitemap_response_code404
-      assert_equal '404', Sitemap.fetch('https://404.example.com/sitemap.xml').code
+      assert_equal '404', @sitemap.fetch(@invalid_url).code
     end
   end
 end
